60b6ea617c7563428b6e4c935ad300a4cb92186f
ashyasingh <ashya@bu.edu>
2022-03-01 16:33:59 -0500

    the first problem i think

---
+++ router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'CS412' });
+});
+
+router.post('/post', (req, res, next) => {
+    let word = req.body.string
+    let object = {name: word, stringlength: word.length}
+    res.render('index', {newdata: JSON.stringify(object)});
+
+});
+
+router.get('/get', function (req, res) {
+    const json = {key: 'Pug is Jade' }; // turns it into a string
+    res.render('index', {data: JSON.stringify(json) }); //turns it into JSON readable format
+
+});
+
+module.exports = router;
