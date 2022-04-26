cd8ff23730baa46ab3f0e28d073cd138f1f0a4a5
ashyasingh <74160572+ashyasingh@users.noreply.github.com>
2022-04-26 17:39:16 -0400

    Add files via upload

---
+++ routes/router.js
@@ -0,0 +1,90 @@
+let express = require('express');
+let router = express.Router();
+const request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    console.log(req.body)
+    res.render('index', { title: 'Country' });
+});
+
+//POST method for CAPITAL
+//callback
+router.post('/capital', (req, res, next) => {
+    let url = `https://restcountries.com/v3.1/name/${req.body.country}`
+    const requestOptions = {
+        url: url,
+        method: 'GET',
+        json: {},
+        qs: {
+            offset: 20
+        }
+    };
+    console.log(url)
+    request(requestOptions, (err, response, body) => {
+        if (err) {
+            console.log(err);
+        } else if (response.statusCode === 200) {
+            let capital = body[0].capital[0];
+            res.render('index', { capital: `${req.body.country}'s capital is ${capital}`, country: req.body.country, title: 'Country'});
+        } else {
+            console.log(response.statusCode);
+        }
+    });
+});
+
+//POST method for CURRENCY
+//callback
+router.post('/currency', (req, res, next) => {
+    let url = `https://restcountries.com/v3.1/name/${req.body.country}`
+    const requestOptions = {
+        url: url,
+        method: 'GET',
+        json: {},
+        qs: {
+            offset: 20
+        }
+    };
+    console.log(url)
+    request(requestOptions, (err, response, body) => {
+        if (err) {
+            console.log(err);
+        } else if (response.statusCode === 200) {
+            let currency = body[0].currency;
+            res.render('index', { currency: `${req.body.country}'s currency is ${currency}`, country: req.body.country, title: 'Country'});
+        } else {
+            console.log(response.statusCode);
+        }
+    });
+});
+
+//POST method for POPULATION
+//callback
+router.post('/population', (req, res, next) => {
+    let url = `https://restcountries.com/v3.1/name/${req.body.country}`
+    const requestOptions = {
+        url: url,
+        method: 'GET',
+        json: {},
+        qs: {
+            offset: 20
+        }
+    };
+    console.log(url)
+    request(requestOptions, (err, response, body) => {
+        if (err) {
+            console.log(err);
+        } else if (response.statusCode === 200) {
+            let population = body[0].population;
+            res.render('index', { population: `${req.body.country}'s population is ${population}`, country: req.body.country, title: 'Country'});
+        } else {
+            console.log(response.statusCode);
+        }
+    });
+});
+
+router.use(express.urlencoded({
+    extended: true
+}))
+
+module.exports = router;
